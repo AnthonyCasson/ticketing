0460d8ff31e08641c1969f4c3748976c3536b73f
AnthonyCasson <anthony@udoc.nl>
2022-06-16 15:22:33 +0200

    section 7 finished

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,5 +1,8 @@
 import { json } from "body-parser";
+import 'express-async-errors';
 import express from "express";
+import { NotFoundError } from "./errors/not-found-error";
+import { errorHandler } from "./middlewares/error-handler";
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
@@ -8,11 +11,17 @@ import { signupRouter } from "./routes/signup";
 const app = express();
 app.use(json());
 
-app.use(currentUserRouter)
-app.use(signinRouter)
-app.use(signoutRouter)
-app.use(signupRouter)
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000!!!!");
+  console.log("Server is running on port 3000!!!!");
 });
