79dadd72349565a37fb80b1d2e8351cd38c561bb
AnthonyCasson <anthony@udoc.nl>
2022-06-17 11:21:05 +0200

    added password hashing

--- auth/src/models.ts/user.ts
+++ auth/src/models.ts/user.ts
@@ -1,13 +1,13 @@
 import mongoose from "mongoose";
+import { Password } from "../services/password";
 
 //interface for the props that are needed to make a new User
 interface UserAttrs {
-  email: string;
+  email: string,
   password: string;
 }
 
-//interface that tells typescript that User has function called build() that takes
-//types Userattrs
+//interface that tells typescript that User has method called build() that takes types Userattrs
 interface UserModel extends mongoose.Model<UserDoc> {
   build(attrs: UserAttrs): UserDoc;
 }
@@ -29,6 +29,15 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+userSchema.pre('save', async function(done) {
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'))
+    this.set('password', hashed)
+  }
+  done()
+})
+
+//adds the build method to the User through the statics mongoose method
 userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs);
 };
