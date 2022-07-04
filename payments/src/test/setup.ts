6e70d322aa3d79862895f122f1a0de6eb9adb14f
AnthonyCasson <anthony@udoc.nl>
2022-07-04 11:33:38 +0200

    installed stripe and created a secret key

--- payments/src/test/setup.ts
+++ payments/src/test/setup.ts
@@ -3,55 +3,55 @@ import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 
 declare global {
-    var signin: () => string[];
+  var signin: (id?: string) => string[];
 }
 
 jest.mock('../nats-wrapper');
 
 let mongo: any;
 beforeAll(async () => {
-    process.env.JWT_KEY = 'asdcscf';
+  process.env.JWT_KEY = 'asdcscf';
 
-    mongo = new MongoMemoryServer();
-    await mongo.start();
-    const mongoUri = await mongo.getUri();
+  mongo = new MongoMemoryServer();
+  await mongo.start();
+  const mongoUri = await mongo.getUri();
 
-    await mongoose.connect(mongoUri);
+  await mongoose.connect(mongoUri);
 });
 
 beforeEach(async () => {
-    jest.clearAllMocks();
-    const collections = await mongoose.connection.db.collections();
+  jest.clearAllMocks();
+  const collections = await mongoose.connection.db.collections();
 
-    for (let collection of collections) {
-        await collection.deleteMany({});
-    }
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
 });
 
 afterAll(async () => {
-    await mongo.stop();
-    await mongoose.connection.close();
+  await mongo.stop();
+  await mongoose.connection.close();
 });
 
-global.signin = () => {
-    //build JWT payload { id, email }
-    const payload = {
-        id: new mongoose.Types.ObjectId().toHexString(),
-        email: 'test@test.com',
-    };
+global.signin = (id?: string) => {
+  //build JWT payload { id, email }
+  const payload = {
+    id: id || new mongoose.Types.ObjectId().toHexString(),
+    email: 'test@test.com',
+  };
 
-    //create the JWT
-    const token = jwt.sign(payload, process.env.JWT_KEY!);
+  //create the JWT
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
 
-    //build session object { jwt: MY_JWT }
-    const session = { jwt: token };
+  //build session object { jwt: MY_JWT }
+  const session = { jwt: token };
 
-    //make object JSON
-    const sessionJSON = JSON.stringify(session);
+  //make object JSON
+  const sessionJSON = JSON.stringify(session);
 
-    //take JSON and encode it as base64
-    const base64 = Buffer.from(sessionJSON).toString('base64');
+  //take JSON and encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString('base64');
 
-    //return a string thats the cookie with the encoded data
-    return [`session=${base64}`];
+  //return a string thats the cookie with the encoded data
+  return [`session=${base64}`];
 };
